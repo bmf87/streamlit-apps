7a3e9714937f203e52757517f7ec987076118376
Brett Favro <59139234+bmf87@users.noreply.github.com>
2025-04-16 19:01:30 +0000

    Minor updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from openai import OpenAI
 
 # Show title and description.
-st.title("📄 Document question answering")
+st.title("📄 Document QA using OpenAI")
 st.write(
     "Upload a document below and ask a question about it – GPT will answer! "
     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
@@ -44,10 +44,14 @@ else:
 
         # Generate an answer using the OpenAI API.
         stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
+            model="gpt-4.1-nano",
             messages=messages,
             stream=True,
         )
 
+        st.write(
+            "ChapGPT's answer to *your* question :sunglasses:",
+            unsafe_allow_html=False,
+        )
         # Stream the response to the app using `st.write_stream`.
         st.write_stream(stream)
